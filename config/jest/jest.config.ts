2edcff2b3b059ed37eaf667fec728cfb2f6d82ad
klownjf1 <mastervinaka@gmail.com>
2023-07-03 01:57:37 +0300

    add redux

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -16,6 +16,9 @@ export default {
     // cacheDirectory: "C:\\Users\\maste\\AppData\\Local\\Temp\\jest",
 
     // Automatically clear mock calls, instances and results before every test
+    globals: {
+        __IS_DEV__: true,
+    },
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
@@ -31,9 +34,9 @@ export default {
     ],
     moduleDirectories: ['node_modules', 'src'],
 
-    /* modulePath: [
+    modulePath: [
         '<rootDir/>src',
-    ], */
+    ],
     testMatch: [
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
@@ -88,7 +91,7 @@ export default {
     // globalTeardown: undefined,
 
     // A set of global variables that need to be available in all test environments
-    // globals: {},
+    //
 
     // eslint-disable-next-line max-len
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
