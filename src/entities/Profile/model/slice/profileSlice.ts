5218232d3a96be9cc9cb277c752945c74871a2e8
klownjf1 <mastervinaka@gmail.com>
2023-07-08 23:55:44 +0300

    fix login form, add profile page and profile Reducer

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,5 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
-import {Profile, ProfileSchema} from '../types/Profile';
+import { ProfileSchema } from '../types/profile';
 
 const initialState: ProfileSchema = {
     readonly: true,
